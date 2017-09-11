2c2fe1a57abd21902202309ce1182b754727d64d
abdullah zuaytir <zaeter2015@gmail.com>
2017-09-11 10:33:35 +0300

    Update cpu-miner.c

--- cpu-miner.c
+++ cpu-miner.c
@@ -682,13 +682,12 @@ static bool get_upstream_work(CURL *curl, struct work *work) {
     gettimeofday(&tv_start, NULL );
 
     if(jsonrpc_2) {
-        char s[128];
-        snprintf(s, 128, "{\"method\": \"getjob\", \"params\": {\"id\": \"%s\"}, \"id\":1}\r\n", rpc2_id);
-        val = json_rpc2_call(curl, rpc_url, rpc_userpass, s, NULL, 0);
-    } else {
-        val = json_rpc_call(curl, rpc_url, rpc_userpass, rpc_req, NULL, 0);
-    }
-    gettimeofday(&tv_end, NULL );
+            char s[128];
+            snprintf(s, 128, "{\"method\": \"getjob\", \"params\": {\"id\": \"%s\"}, \"id\":1}\r\n", rpc2_id);
+            val = json_rpc2_call(curl, rpc_url, rpc_userpass, s, NULL, JSON_RPC_LONGPOLL);
+        } else {
+            val = json_rpc_call(curl, rpc_url, rpc_userpass, rpc_req, NULL, JSON_RPC_LONGPOLL);
+        }
 
     if (have_stratum) {
         if (val)
